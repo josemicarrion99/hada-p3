cb4996e53e3cbb95e379dfd015011f839db3c88e
Josemi <jmcp25@alu.ua.es>
2021-03-23 10:35:49 +0100

    Agregar archivos de proyecto.

---
+++ usuWeb/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="usuWeb.Formulario_web1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <h1>Página de usuarios</h1>
+        <div>
+            <p>
+                NIF: <asp:TextBox ID="NIFTextBox" TextMode="SingleLine" 
+                    Columns="9" runat="server" /> 
+            </p>
+            <p>
+                Nombre: <asp:TextBox ID="NombreTextBox" TextMode="SingleLine" 
+                    Columns="30" runat="server" /> 
+            </p>
+            <p>
+                Edad: <asp:TextBox ID="EdadTextBox" TextMode="SingleLine" 
+                    Columns="3" runat="server"/> 
+            </p>
+
+            <asp:Button ID="BotonLeerUsuario" Text="Leer" runat="server" OnClick="LeerUsuario" />
+            <asp:Button ID="BotonLeerPrimerUsuario" Text="Leer Primero" runat="server" OnClick="LeerPrimerUsuario" />
+            <asp:Button ID="BotonLeerAnteriorUsuario" Text="Leer Anterior" runat="server" OnClick="LeerAnteriorUsuario" />
+            <asp:Button ID="BotonLeerSiguienteUsuario" Text="Leer Siguiente" runat="server" OnClick="LeerSiguienteUsuario" />
+            <asp:Button ID="BotonCrearUsuario" Text="Crear" runat="server" OnClick="CrearUsuario" />
+            <asp:Button ID="BotonActualizar" Text="Actualizar" runat="server" OnClick="Actualizar" />
+            <asp:Button ID="BotonBorrarUsuario" Text="Borrar" runat="server" OnClick="BorrarUsuario" />
+
+
+        </div>
+</asp:Content>
---
+++ usuWeb/Default.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace usuWeb
+{
+
+
+    public partial class Formulario_web1
+    {
+
+        /// <summary>
+        /// Control NIFTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NIFTextBox;
+
+        /// <summary>
+        /// Control NombreTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NombreTextBox;
+
+        /// <summary>
+        /// Control EdadTextBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox EdadTextBox;
+
+        /// <summary>
+        /// Control BotonLeerUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerUsuario;
+
+        /// <summary>
+        /// Control BotonLeerPrimerUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerPrimerUsuario;
+
+        /// <summary>
+        /// Control BotonLeerAnteriorUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerAnteriorUsuario;
+
+        /// <summary>
+        /// Control BotonLeerSiguienteUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonLeerSiguienteUsuario;
+
+        /// <summary>
+        /// Control BotonCrearUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCrearUsuario;
+
+        /// <summary>
+        /// Control BotonActualizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonActualizar;
+
+        /// <summary>
+        /// Control BotonBorrarUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonBorrarUsuario;
+    }
+}
